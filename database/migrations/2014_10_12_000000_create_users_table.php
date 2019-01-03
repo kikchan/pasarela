f3152be1dd668885150a101c541115c5507963eb
arm149 <arm149@alu.ua.es>
2019-01-03 21:56:49 +0100

    Primera migración de Usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('nombre');
+			$table->string('apellidos');
+			$table->string('nick')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+			$table->boolean('esComercio')->default('1');
+			$table->boolean('esAdministrador')->default('0');
+			$table->boolean('esTecnico')->default('0');
             $table->timestamps();
         });
     }
