038e86cd70429cfbb6c1173f9a578c05b56a166c
Wing Kei Liang Tan <wklt1@alu.ua.es>
2019-01-10 00:17:30 +0100

    Cambiado ruta del css

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Pasarela - Comercio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/comercio.css">
+        <link href="{{ asset('css/comercio.css') }}" rel="stylesheet">
         
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
