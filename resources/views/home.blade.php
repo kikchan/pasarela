aa0ebdb1c2a6a1ee840982e9413283af32642a5e
Kiril Gaydarov <kvg1@alu.ua.es>
2019-01-17 16:28:41 +0100

    Home mockeado, añadido nombre de usuario en el navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Backoffice</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Bienvenid@ al Backoffice de <b>{{config('app.name')}}</b>!
                 </div>
             </div>
         </div>
